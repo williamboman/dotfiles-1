08d7f5dcb272e8b0183efda25cdf1dc280360833
Jongwook Choi <wookayin@gmail.com>
2021-02-25 17:27:16 -0800

    Experimental.

--- zsh/zsh.d/fzf-widgets.zsh
+++ zsh/zsh.d/fzf-widgets.zsh
@@ -6,3 +6,26 @@
 
 # More Shortcuts
 bindkey '^ ' fzf-file-widget          # Ctrl-SPACE, Ctrl-T
+
+() { return;
+
+# CTRL-V: fill 'vim ...' command with fzf select
+zle      -N   fzf-vimfile-widget
+bindkey  '^V' fzf-vimfile-widget
+fzf-vimfile-widget() {
+  if [[ -n "$LBUFFER" && "$LBUFFER" != "vim " ]]; then
+    return 1   # works only if the cmdline is empty
+  fi
+  LBUFFER="vim "
+  zle reset-prompt
+  selected="$(__fsel --prompt "vim> ")"
+  if [[ -n "$selected" ]]; then
+    ; #eval "vim ${selected[@]}"
+  fi
+  local ret=$?
+  LBUFFER="vim ${selected}"
+  zle reset-prompt
+  return $ret
+}
+
+}
